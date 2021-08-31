98505085a8fbd49afbe7599dc2b1109cfb16d15f
Kapustin Alexander <akapustin@ambrahealth.com>
2021-08-31 18:55:42 +0300

    Add doc for updating textures

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,5 +1,14 @@
 #include "texture.h"
 
+// In modern OpenGL there are 4 different methods to update 2D textures:
+//
+// - glTexImage2D - the slowest one, recreates internal data structures.
+// - glTexSubImage2D - a bit faster, but can't change the parameters (size, pixel format) of the image.
+// - Render-to-texture with FBO - update texture entirely on GPU, very fast. Refer to this answer for more details: https://stackoverflow.com/a/10702468/1065190
+// - Pixel Buffer Object PBO - for fast uploads from CPU to GPU, not supported (yet) on OpenGL ES.
+
+
+
 Texture::Texture(const char* imagePath, const char* textureName, GLenum glTextureId):
     imagePath(imagePath), textureName(textureName), glTextureId(glTextureId)
 {
@@ -20,7 +29,7 @@ Texture::Texture(const char* imagePath, const char* textureName, GLenum glTextur
 }
 
 void Texture::activate() {
-  glActiveTexture(GL_TEXTURE0);
+  // glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, textureId);
 }
 
